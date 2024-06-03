597f4052ac48f159402994ae36c8fe9828a36ac5
Bram <84628826+sys-256@users.noreply.github.com>
2024-06-03 22:19:22 +0200

    Arduino IDE -> VSCode

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # morse-arduino
 
-Link voor Regexp.h: https://github.com/nickgammon/Regexp
+- Link voor Regexp.h: https://github.com/nickgammon/Regexp
+- Bespreken:
+	- Regex weghalen
+	- Layout knopjes
+	- É weghalen
+	- char Values[] -> string Values[] met echte Arduino
--- morse-arduino.ino
+++ morse-arduino.ino
@@ -1,55 +1,61 @@
 #include <Regexp.h>
 
-void setup() {
-  Serial.begin(9600);
+void setup()
+{
+	Serial.begin(9600);
 }
 
-const String morseToTranslate = "- . ... - / ...- ---fsdf1kl2k --- .-. / .. -. ..-. --- asxz.-. -- .-fdsjcxz1$*( - .. -.-. .-";
-const String Keys[51] = { ".-", "-...", "-.-.", "-..", ".", "..-..", "..-.", "--.", "....", "..", ".---",
-                          "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-",
-                          "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-",
-                          ".....", "-....", "--...", "---..", "----.", "-----", ".-.-.-", "--..--",
-                          "---...", "..--..", ".----.", "-....-", "-..-.", "-.--.", "-.--.-",  ".-..-.",
-                          "-...-", ".-.-.", ".--.-.", "/" };
-const char Values[51] = { 'A', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
-                          'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5',
-                          '6', '7', '8', '9', '0', '.', ',', ':', '?', '"', '-', '/', '(', ')', '"',
-                          '=', '+', '@', ' ' };
-
-void loop() {
-  Serial.println(morseToTranslate);
-  Serial.println(morseToText(morseToTranslate));
-  Serial.println();
-  delay(1000);
+void loop()
+{
+	Serial.println(morseToTranslate);
+	Serial.println(morseToText(morseToTranslate));
+	Serial.println();
+	delay(1000);
 }
 
-String morseToText(String input) {
-  /* Convert to buffer */
-  char buffer[input.length() +1];
-  input.toCharArray(buffer, input.length() + 1);
+const String morseToTranslate = ".-..-. - . ... - .-..-. fsdkljfk -..-. ..--- ---.. ----. ...-- ---... / -.--. -...- 239402#sdo .-.-.- ..-.. -.--.-";
+const String Keys[51] = {".-", "-...", "-.-.", "-..", ".", "..-..", "..-.", "--.", "....", "..", ".---",
+						 "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-",
+						 "...-", ".--", "-..-", "-.--", "--..", ".----", "..---", "...--", "....-",
+						 ".....", "-....", "--...", "---..", "----.", "-----", ".-.-.-", "--..--",
+						 "---...", "..--..", ".----.", "-....-", "-..-.", "-.--.", "-.--.-", ".-..-.",
+						 "-...-", ".-.-.", ".--.-.", "/"};
+const char Values[51] = {'A', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O',
+						 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5',
+						 '6', '7', '8', '9', '0', '.', ',', ':', '?', '"', '-', '/', '(', ')', '"',
+						 '=', '+', '@', ' '};
 
-  /* Apply regex statement */
-  MatchState ms(buffer);
-  ms.GlobalReplace("[^-./ ]", "");
+String morseToText(String input)
+{
+	/* Convert to buffer */
+	char buffer[input.length() + 1];
+	input.toCharArray(buffer, input.length() + 1);
 
-  /* Split every space */
-  char *currentSplit;
-  String result = "";
-  currentSplit = strtok(buffer, " ");
-  while (currentSplit != NULL) {
-    /* Find the index of the key */
-    int keyIndex;
-    for (int i = 0; i < 51; ++i) {
-      if (Keys[i].equals(String(currentSplit))) {
-        keyIndex = i;
-        break;
-      }
-    }
+	/* Apply regex statement */
+	MatchState ms(buffer);
+	ms.GlobalReplace("[^-./ ]", "");
 
-    /* Add the value to the result string */
-    result += String(Values[keyIndex]);
-    currentSplit = strtok(NULL, " ");
-  }
+	/* Split at every space */
+	char *currentSplit;
+	String result = "";
+	currentSplit = strtok(buffer, " ");
+	while (currentSplit != NULL)
+	{
+		/* Find the index of the key */
+		int keyIndex;
+		for (int i = 0; i < 51; ++i)
+		{
+			if (Keys[i].equals(String(currentSplit)))
+			{
+				keyIndex = i;
+				break;
+			}
+		}
 
-  return result;
+		/* Add the value to the result */
+		result += String(Values[keyIndex]);
+		currentSplit = strtok(NULL, " ");
+	}
+
+	return result;
 }
