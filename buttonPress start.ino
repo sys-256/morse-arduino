715fa0346b69406aa5d1306df13777b980547de9
Mika Bar DEV <89031266+MikaBarDEV@users.noreply.github.com>
2024-06-06 15:26:35 +0200

    Add string concat

--- buttonPress start.ino
+++ buttonPress start.ino
@@ -1,8 +1,7 @@
 const int buttonPin = 2;
 
-
-int buttonState = 0;
 long lastTimeChanged = 0;
+String dataToMorse = ""
 
 void setup() {
   pinMode(buttonPin, INPUT);	
@@ -16,17 +15,21 @@ void loop() {
 
     while(digitalRead(buttonPin) == 1){};
 
-
-    if(millis() - lastTimeChanged < 250){
-        Serial.println("Short");
-
-      }else if(millis() - lastTimeChanged < 500){
-        Serial.println("Long");
-
-      }else if(millis() - lastTimeChanged < 2000){
-        Serial.println("Space");
-
-      }
+    if(millis() - lastTimeChanged < 15){
+      Serial.println("False Registery");
+    }else if(millis() - lastTimeChanged < 250){
+      Serial.println("Short");
+      dataToMorse = dataToMorse + ".";
+    }else if(millis() - lastTimeChanged < 1000){
+      Serial.println("Long");
+      dataToMorse = dataToMorse + "-";
+    }else if(millis() - lastTimeChanged < 4000){
+      Serial.println("nieuwe letter");
+      dataToMorse = dataToMorse + " ";
+    }else{
+      Serial.println("Space");     
+      dataToMorse = dataToMorse + "/";   
+    };
 
 
     
